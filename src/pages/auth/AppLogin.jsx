91dc26b4b135097a9be43e7d141e5828964b6eca
Leograzioli <leograzioli@gmail.com>
2023-06-05 12:56:49 +0200

    add validation

--- src/pages/auth/AppLogin.jsx
+++ src/pages/auth/AppLogin.jsx
@@ -6,23 +6,34 @@ import { Link, useNavigate } from "react-router-dom";
 export default function AppLogin() {
     const [email, setEmail] = useState('mariorossi@gmail.com')
     const [password, setPassword] = useState('123123123')
+    const [errors, setErrors] = useState({})
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setLoading(true)
 
         axios.post('http://127.0.0.1:8000/api/auth/login', {
             email,
             password
 
         }).then(resp => {
-            console.log(resp.data);
+
             if (resp.data.status) {
                 navigate('/adm')
                 Cookies.set('token', resp.data.token, { expires: 1 / 24 })
                 Cookies.set('userName', resp.data.user.name, { expires: 1 / 24 } )
             }
-        })
+
+        }).catch(err => {
+
+            setErrors(err.response.data.error);
+
+        }).finally( () => {
+
+            setLoading(false)
+        } )
     }
 
     return (
@@ -36,19 +47,25 @@ export default function AppLogin() {
                             <div className="mt-3 mx-auto w-full">
                                 <label >
                                     <div className="text-xl">Email</div>
-                                    <input onChange={(e) => setEmail(e.target.value)} value={'mariorossi@gmail.com'} type="text" className="rounded-md py-2 px-4 w-full mt-1" />
+                                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="text" className={`${errors.email? 'border-2 border-red-500' : ''} rounded-md py-2 px-4 w-full mt-1`} />
+                                    {errors.email && errors.email.map( err => (
+                                        <span key={err} className="text-red-500 text-sm">-{err}</span>
+                                    ))}
                                 </label>
                             </div>
                             <div className="mt-3 mx-auto w-full">
                                 <label >
                                     <div className="text-xl">Password</div>
-                                    <input onChange={(e) => setPassword(e.target.value)} type="password" value={"123123123"} className="rounded-md py-2 px-4 w-full mt-1" />
+                                    <input onChange={(e) => setPassword(e.target.value)} type="password" value={password} className={`${errors.password? 'border-2 border-red-500' : ''} rounded-md py-2 px-4 w-full mt-1`} />
+                                    {errors.password && errors.password.map( err => (
+                                        <span key={err} className="text-red-500 text-sm">-{err}</span>
+                                    ))}
                                 </label>
                             </div>
 
                             <Link to={'/register'} className="mx-auto mt-4 text-sm underline text-blue-700">Not registered yet?</Link>
 
-                            <button className="mt-4 bg-white rounded-md py-1 w-1/3 mx-auto font-semibold">Login</button>
+                            <button className="mt-4 bg-white rounded-md py-1 w-1/3 mx-auto font-semibold">{loading? 'loading..' : 'Login'}</button>
                         </form>
 
                     </div>
